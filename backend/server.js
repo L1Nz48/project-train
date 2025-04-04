c843373ee030cf487ad3170d8c3579890840ea6f
L1Nz48 <nuerkung92@gmail.com>
2025-04-04 14:14:09 +0700

    change useUnifiedTopology

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,6 @@ app.use(express.json());
 // เชื่อมต่อ MongoDB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
 }).then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
