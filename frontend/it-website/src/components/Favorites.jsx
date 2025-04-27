76bac1130facb6c93d5c42ec288e494c54f91da5
L1Nz48 <nuerkung92@gmail.com>
2025-04-28 00:35:34 +0700

    add ToastContainer

--- frontend/it-website/src/components/Favorites.jsx
+++ frontend/it-website/src/components/Favorites.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
+import { toast,ToastContainer } from 'react-toastify';
 import Loading from './Loading';
 
 
